2025367d4c1e620bf5593e40f964d005355185d0
ayaz-khatri <ayaz.khatri919@gmail.com>
2025-10-26 20:24:24 +0500

    Basic CRUD operations completed."

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-import express from 'express';       
-const app = express();                                        
+const express = require('express');
+const mongoose = require('mongoose');
+const Contact = require('./models/contacts.model.js');
+const app = express();  
+
+mongoose.connect('mongodb://127.0.0.1:27017/contacts-crud')
+.then(() => {console.log("Database Connected.")});
 
 /* ------------------------------- Middlewares ------------------------------ */
 
@@ -11,28 +16,48 @@ app.use(express.static('public'));              // set the default folder for st
 
 /* --------------------------------- Routes --------------------------------- */
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.get('/', async (req, res) => {
+    const contacts = await Contact.find();
+    res.render('home', {contacts});
 });
 
-app.get('/show-contact', (req, res) => {
-    res.render('show-contact');
+app.get('/show-contact/:id', async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    res.render('show-contact', {contact});
 });
 
 app.get('/add-contact', (req, res) => {
     res.render('add-contact');
 });
 
-app.post('/add-contact', (req, res) => {});
+app.post('/add-contact', async (req, res) => {
+    // const contact = await Contact.insertOne({
+    //     first_name: req.body.first_name,
+    //     last_name: req.body.last_name,
+    //     email: req.body.email,
+    //     phone: req.body.phone,
+    //     address: req.body.address,
+    // });
 
-app.get('/update-contact', (req, res) => {
-    res.render('update-contact');
+    // this used when form-fields and database-fields names are same
+    await Contact.create(req.body); // create is mongoose method
+    res.redirect('/');
 });
 
-app.post('/update-contact', (req, res) => {});
+app.get('/update-contact/:id', async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    res.render('update-contact', {contact});
+});
 
-app.post('/delete-contact', (req, res) => {});
+app.post('/update-contact/:id', async (req, res) => {
+    await Contact.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect('/');
+});
 
+app.get('/delete-contact/:id', async (req, res) => {
+    await Contact.findByIdAndDelete(req.params.id);
+    res.redirect('/');
+});
 
 /* ---------------------------- Start the Server ---------------------------- */
 app.listen(3000, () => {   
